Team-Hydra/android_device_oneplus_sm8150-common

sm8150-common: DeviceSettings: Add a scrollable image view layout
- from Havoc-Devices/android_device_oneplus_settings@720debd

Derpfest edits:
- Migrate to androidx

EvoX edits:
- Adapt to DeviceSettings
- Swap display mode preview images 1 & 2

Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>
Signed-off-by: AnierinB <anierinb@evolution-x.org>

DeviceSettings/src/com/yaap/device/DeviceSettings/ViewPagerAdapter.java
@@ -1,0 +1,62 @@
+/*
+* Copyright (C) 2018 Havoc-OS
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.yaap.device.DeviceSettings;
+
+import android.content.Context;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+public class ViewPagerAdapter extends PagerAdapter {
+
+    private Context context;
+    private LayoutInflater layoutInflater;
+    private Integer [] images = {R.drawable.image1,R.drawable.image2,R.drawable.image3};
+
+    public ViewPagerAdapter(Context context) {
+        this.context = context;
+    }
+
+    public int getCount() {
+        return images.length;
+    }
+
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    public Object instantiateItem(ViewGroup container, final int position) {
+        layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.image_layout, null);
+        ImageView imageView = (ImageView) view.findViewById(R.id.imageView);
+        imageView.setImageResource(images[position]);
+
+        ViewPager vp = (ViewPager) container;
+        vp.addView(view, 0);
+        return view;
+    }
+
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        ViewPager vp = (ViewPager) container;
+        View view = (View) object;
+        vp.removeView(view);
+    }
+}

crazyinsanejames/device_oneplus_sm8150-Just-Building

DeviceSettings/src/org/lineageos/device/DeviceSettings/ViewPagerAdapter.java
@@ -1,0 +1,62 @@
+/*
+* Copyright (C) 2018 Havoc-OS
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.lineageos.device.DeviceSettings;
+
+import android.content.Context;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+public class ViewPagerAdapter extends PagerAdapter {
+
+    private Context context;
+    private LayoutInflater layoutInflater;
+    private Integer [] images = {R.drawable.image1,R.drawable.image2,R.drawable.image3};
+
+    public ViewPagerAdapter(Context context) {
+        this.context = context;
+    }
+
+    public int getCount() {
+        return images.length;
+    }
+
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    public Object instantiateItem(ViewGroup container, final int position) {
+        layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.image_layout, null);
+        ImageView imageView = (ImageView) view.findViewById(R.id.imageView);
+        imageView.setImageResource(images[position]);
+
+        ViewPager vp = (ViewPager) container;
+        vp.addView(view, 0);
+        return view;
+    }
+
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        ViewPager vp = (ViewPager) container;
+        View view = (View) object;
+        vp.removeView(view);
+    }
+}