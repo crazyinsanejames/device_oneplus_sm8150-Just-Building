9035b64ae2514fa6b0827154885d9386ae13dd81
timocapa <timo.capa@gmail.com>
2021-10-27 17:00:53 +0200

    sm8150-common: Enable Vulkan
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -115,6 +115,9 @@ DEVICE_MATRIX_FILE := $(VENDOR_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(VENDOR_PATH)/manifest.xml
 ODM_MANIFEST_FILES := $(VENDOR_PATH)/manifest-qva.xml
 
+# Graphics
+TARGET_USES_VULKAN := true
+
 # Init
 TARGET_INIT_VENDOR_LIB := //$(VENDOR_PATH):libinit_oneplus-sm8150
 TARGET_RECOVERY_DEVICE_MODULES := libinit_oneplus-sm8150
